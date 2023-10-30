bbe0b1de56d58fb6a6e768cda4bfb5b03d0fcc78
Otgontseren Radnaabazar <LP230109@G301-CC0180.local>
2023-10-30 18:02:00 +0800

    get put delete post duusgaw

--- lesson3_server/app1.js
+++ lesson3_server/app1.js
@@ -4,16 +4,30 @@ const fs = require("fs");
 
 const {v4: uuidv4} = require("uuid")
 
-const app = express();
-
 const PORT = 8008;
+
+const app = express();
 
 app.use(express.json());
 
 app.get("/api/users", (req,res) => {
-    console.log("Get all users");
-    res.status(200).json({message: "Get success"});
+    const {users} = JSON.parse(
+        fs.readFileSync("users.json", {encoding: "utf-8"}));
+        res.status(200).json({message: "Get message" ,users});
 });
+
+app.get("/api/users/:usersId" , (req,res) => {
+    console.log("get an user by id");
+    const {users} = JSON.parse(
+        fs.readFileSync("users.json", {encoding: "utf-8"}));
+    const findUser = users.filter((user) => user.id === req.params.usersId);
+    if(findUser.length === 0 ){
+        res.status(400).json({message: `${userId} тай хэрэглэгч олдсонгүй`})
+    }else {
+    res.status(200).json({message: "Get message" ,user: findUser[0]});
+
+}})
+
 app.post("/api/users", (req,res) => {
     console.log("Create new user");
     
@@ -30,11 +44,28 @@ app.post("/api/users", (req,res) => {
 
     res.status(201).json({message: "Post success"});
 });
-app.put("/api/users/:id", (req,res) => {
+app.put("/api/users/:userId", (req,res) => {
     console.log("Put user by id");
-    res.status(200).json({message: "Put success"});
+    const { userId } = req.params;
+    const { users } = JSON.parse(
+        fs.readFileSync("users.json", { encoding: "utf-8" })
+          );
+    let index = users.findIndex((user) => user.id === userId);
+
+    if (index === -1) {
+        res.status(400).json({ message: `${userId} тай хэрэглэгч олдсонгүй.` });
+    } else {
+    users[index] = { ...users[index], ...req.body };
+    fs.writeFileSync("users.json", JSON.stringify({ users }), {
+      encoding: "utf-8",
+    });
+    res.status(200).json({
+      message: `${userId} тай хэрэглэгчийг шинэчиллээ.`,
+      user: users[index],
+    });
+  }
 });
-app.delete("/api/users/:id", (req,res) => {
+app.delete("/api/users/:userId", (req,res) => {
     console.log("Delete user by id");
 
     const {userId} = req.params;
