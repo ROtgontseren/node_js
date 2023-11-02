dd93bb786500d4943f3ee5c39cb89eaedad3bd8e
unknown <otgoohghg@yahoo.com>
2023-11-02 13:47:22 +0800

    gert zaswar 1

--- lesson4_mvc/controller/userController.js
+++ lesson4_mvc/controller/userController.js
@@ -1,81 +1,81 @@
 const { v4: uuidv4 } = require("uuid");
-const fs = require("fs");
+const { readFile, writeFile } = require("../utils/fileHandler");
 
 const getAllUser = (req, res) => {
-  console.log("Get all users");
-  const { users } = JSON.parse(
-    fs.readFileSync("users.json", { encoding: "utf-8" })
-  );
-  res.status(200).json({ message: "success", users });
+  console.log("Get all `users");
+  try {
+    const users = readFile("users.json");
+    return res.status(200).json({ message: "success", users });
+  } catch (error) {
+    return res.status(error.statusCode).json({ message: error.message });
+  }
 };
 
 const getUserById = (req, res) => {
   console.log("Get an user by ID");
-  const { users } = JSON.parse(
-    fs.readFileSync("users.json", { encoding: "utf-8" })
-  );
-  const findUser = users.filter((user) => user.id === req.params.userId);
-  if (findUser.length === 0) {
-    res.status(400).json({ message: `${userId} тай хэрэглэгч олдсонгүй.` });
-  } else {
-    res.status(200).json({ message: "success", user: findUser[0] });
+
+  try {
+    const users = readFile("users.json");
+    const findUser = users.filter((user) => user.id === req.params.userId);
+    if (findUser.length === 0) {
+      res.status(400).json({ message: `${userId} тай хэрэглэгч олдсонгүй.` });
+    } else {
+      res.status(200).json({ message: "success", user: findUser[0] });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
   }
 };
 
 const createUser = (req, res) => {
-  console.log("Create new user");
-  const newUser = { id: uuidv4(), ...req.body };
-
-  const { users } = JSON.parse(
-    fs.readFileSync("users.json", { encoding: "utf-8" })
-  ); // {users: []}
-
-  users.push(newUser);
-
-  fs.writeFileSync("users.json", JSON.stringify({ users }), {
-    encoding: "utf-8",
-  });
-  res.status(201).json({ message: "success" });
+  try {
+    console.log("Create new user");
+    const newUser = { id: uuidv4(), ...req.body };
+    const users = readFile("users.json");
+    users.push(newUser);
+    writeFile("users.json", users);
+    return res.status(201).json({ message: "success" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
 const updateUserById = (req, res) => {
   console.log("Update user by id");
-  const { userId } = req.params;
-  const { users } = JSON.parse(
-    fs.readFileSync("users.json", { encoding: "utf-8" })
-  );
-  let index = users.findIndex((user) => user.id === userId);
-
-  if (index === -1) {
-    res.status(400).json({ message: `${userId} тай хэрэглэгч олдсонгүй.` });
-  } else {
-    users[index] = { ...users[index], ...req.body };
-    fs.writeFileSync("users.json", JSON.stringify({ users }), {
-      encoding: "utf-8",
-    });
-    res.status(200).json({
-      message: `${userId} тай хэрэглэгчийг шинэчиллээ.`,
-      user: users[index],
-    });
+  try {
+    const { userId } = req.params;
+    const users = readFile("users.json");
+    let index = users.findIndex((user) => user.id === userId);
+    if (index === -1) {
+      res.status(400).json({ message: `${userId} тай хэрэглэгч олдсонгүй.` });
+    } else {
+      users[index] = { ...users[index], ...req.body };
+      writeFile("users.json", users);
+      res.status(200).json({
+        message: `${userId} тай хэрэглэгчийг шинэчиллээ.`,
+        user: users[index],
+      });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
   }
 };
 
 const deleteUserById = (req, res) => {
   console.log("Delete user by id");
-  const { userId } = req.params;
-  const { users } = JSON.parse(
-    fs.readFileSync("users.json", { encoding: "utf-8" })
-  );
-  const index = users.findIndex((el) => el.id === userId);
-  if (index < 0) {
-    res.status(400).json({ message: `${userId} тай хэрэглэгч олдсонгүй.` });
-  } else {
-    users.splice(index, 1);
-
-    fs.writeFileSync("users.json", JSON.stringify({ users }), {
-      encoding: "utf-8",
-    });
-    res.status(200).json({ message: `${userId} тай хэрэглэгч устгалаа.` });
+  try {
+    const { userId } = req.params;
+    const users = readFile("users.json");
+    const index = users.findIndex((el) => el.id === userId);
+    if (index < 0) {
+      res.status(400).json({ message: `${userId} тай хэрэглэгч олдсонгүй.` });
+    } else {
+      users.splice(index, 1);
+      writeFile("users.json", users);
+      res.status(200).json({ message: `${userId} тай хэрэглэгч устгалаа.` });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
   }
 };
 
--- lesson4_mvc/middleware/logger.js
+++ lesson4_mvc/middleware/logger.js
@@ -1,9 +1,25 @@
+// const logger = (req, res, next) => {
+//     console.log(
+//       "LOG :" + `${req.method} : ${req.url} : ${new Date().toISOString()}`
+//     );
+  
+//     next();
+//   };
+  
+//   module.exports = { logger };
+const winston = require("winston");
+
 const logger = (req, res, next) => {
-    console.log(
-      "LOG :" + `${req.method} : ${req.url} : ${new Date().toISOString()}`
-    );
-  
-    next();
-  };
-  
-  module.exports = { logger };
+  const logWinston = winston.createLogger({
+    format: winston.format.json(),
+    transports: [new winston.transports.File({ filename: "combined.log" })],
+  });
+  logWinston.log(
+    "info",
+    `${new Date().toLocaleDateString()} - ${req.originalUrl} - ${req.method}`,
+    () => {}
+  );
+  next();
+};
+
+module.exports = { logger };
